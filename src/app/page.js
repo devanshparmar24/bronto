971b29a0954bb14e5347fd0a13216f80ef96e857
devanshparmar24 <devanshparmar13618@gmail.com>
2026-03-05 14:47:56 +0530

    Code readability improvement - all imports of components can be done from index.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,4 @@
-import Architecture from "@/components/Architecture";
-import Capabilities from "@/components/Capabilities";
-import CTA from "@/components/CTA";
-import Footer from "@/components/Footer/Footer";
-import Navbar from "@/components/Header/Navbar";
-import Hero from "@/components/Hero/Hero";
-import Overview from "@/components/Overview";
-import PerformanceStats from "@/components/PerformanceStats";
-import Specs from "@/components/Specs";
+import {Architecture, Capabilities, CTA, Footer, Navbar, Hero, Overview, PerformanceStats, Specs} from "@/components/index"
 export default function Bronto() {
   return (
     <>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,21 @@
 import Hero from "./Hero/Hero";
 import Navbar from "./Header/Navbar"
 import Footer from "./Footer/Footer";
-
-export default {Footer,Navbar, Hero }
+import Architecture from "./Architecture";
+import Capabilities from "./Capabilities";
+import Counter from "./Counter";
+import CTA from "./CTA";
+import Overview from "./Overview";
+import PerformanceStats from "./PerformanceStats";
+import Specs from "./Specs";
+export {Footer,
+        Navbar,
+        Hero,
+        Architecture,
+        Capabilities,
+        Counter,
+        CTA,
+        Overview,
+        PerformanceStats,
+        Specs
+    }
